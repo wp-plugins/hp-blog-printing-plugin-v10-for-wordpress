772c7a0f43940e36e15c53e4ce62f71b77726f39
adamcriticalpathcom <adamcriticalpathcom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-17 20:32:56 +0000

    Updated plugin information to version 1.1.
    git-svn-id: https://plugins.svn.wordpress.org/hp-blog-printing-plugin-v10-for-wordpress/trunk@19187 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- HPBlogPrint.php
+++ HPBlogPrint.php
@@ -4,7 +4,7 @@ Plugin Name: HP Blog Printing
 Plugin URI: http://developer.tabblo.com/index.php/hp-blog-printing/
 Description: Adds a PDF printing button to your WordPress blog.
 Author: Hewlett-Packard
-Version: 1.0.8
+Version: 1.1
 Author URI: http://developer.tabblo.com/
 
 Requires WordPress 2.1 or later.
